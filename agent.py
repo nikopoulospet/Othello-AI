51177edac6d79c9dadae5216b22df40276111e8e
Neville Ingram <nlingram@wpi.edu>
2021-10-04 18:24:53 -0400

    Added comments

--- agent.py
+++ agent.py
@@ -13,7 +13,6 @@ import time
 BOARD_SIZE = 8
 DEPTH_LIMIT = 1
 time_limit = 1000
-wentFirst = False
 movesVisited = {}
 
 
@@ -22,17 +21,13 @@ def main():
     gameboard = npBoard()
     moveTimer = np.array([1])
     while(not gameOver):
-        wentFirst = False
         # if game is over break
         if(os.path.isfile('end_game')):
             gameOver = True
             continue
 
         # if not my turn break
-        if(not os.path.isfile(__file__ + '.go')):
-            # time.sleep(0.05)
-            # maybe add move scanning here to save time?
-            # or start caculating possable furture moves
+        if(not os.path.isfile('agent.go')):
             continue
 
         # read other players move
@@ -45,17 +40,14 @@ def main():
                 line = next_line
 
         # check to see if you are making the first move
-        # aka no move before this one
         if line == "":
-            wentFirst = True
-            # global DEPTH_LIMIT
-            # DEPTH_LIMIT = 3
+            # no move from the opponent, we go first
             gameboard.switchToFirstPlayer()
         else:  # if there is a move that exists from the oponet do it
             # Tokenize move
             tokens = line.split()
             player = tokens[0]
-            if(player == "agent.py"):
+            if(player == "agent"):
                 continue
             col = tokens[1]
             row = tokens[2]
@@ -64,19 +56,15 @@ def main():
                 gameboard.board = npBoard.set_piece_coords(
                     int(row), col, -1, gameboard.board)
 
-        # Find all legal moves
         # move making logic
-        t2_start = process_time_ns()
-        # goodMoves = shallowSearch(gameboard)
         bestMove = miniMax(gameboard)
-        t2_stop = process_time_ns()
-        t2_diff = (t2_stop - t2_start) / 1000000000
-        moveTimer = np.append(moveTimer, [[t2_diff]])
+
+        # make move on the board
         gameboard.board = npBoard.set_piece_index(bestMove, 1, gameboard.board)
 
         # send move
         file = open('move_file', 'w')
-        file.write("agent.py" + npBoard.writeCoords(bestMove))
+        file.write("agent" + npBoard.writeCoords(bestMove))
         file.close()
 
 
@@ -91,43 +79,49 @@ def miniMax(gameboard: npBoard):
     # get legal moves after
     legalMoves = npBoard.getLegalmoves(1, gameboard.getBoard())
 
-    # check to see if passing is needed
+    # check to see if we need to pass
     if len(legalMoves) == 0:
         return -1
 
+    # bestMove is the index of the bestMove
     bestMove = np.inf
+
+    # bestHeuristic is the index of the bestMove
     bestHeuristic = np.NINF
-    # look at all the possible responses we have to the opponents move
-    max_time = int(3)
-    start_time = time.time()  # remember when we started
-    # while (time.time() - start_time) < max_time:
+
+    # start tree with our next possible moves
     for move in legalMoves:
+        # start pruning
         currBest = findMin(
             gameboard.board, bestHeuristic, bestMove, 0, DEPTH_LIMIT)
+
+        # update bestMove and bestHeuristic
         if currBest > bestHeuristic:
             bestHeuristic = currBest
             bestMove = move
+    # return bestMove index
     return bestMove
 
 
 def evaluation(currBoard: npBoard):
     """
     :param currBoard is the current board state
-    :return the heuristic score of the board currently from our POV
+    :return the evaluation score of the board currently from our POV
     """
-    # Legal moves worth 10
-    # Corners worth 100
-    # B2, B7, G2, and G7 worth -25
 
+    #
     if 64 - np.sum(np.abs(currBoard)) <= DEPTH_LIMIT * 2:
         return np.sum(currBoard)
 
+    # weight between our legal moves and theirs, more legal moves is better
     ourLegalMoves = len(npBoard.getLegalmoves(1, currBoard))
     theirLegalMoves = len(npBoard.getLegalmoves(-1, currBoard))
     moveWeight = ourLegalMoves - theirLegalMoves
 
+    # sum board to see who currently has more discs
     discWeight = np.sum(currBoard)
 
+    # based on othello strategy, weight certain spots more than others. Example: corners are good
     spotWeights = np.array([4, -3, 2, 2, 2, 2, -3, 4,
                             -3, -4, -1, -1, -1, -1, -4, -3,
                             2, -1, 1, 0, 0, 1, -1, 2,
@@ -139,11 +133,8 @@ def evaluation(currBoard: npBoard):
 
     spotWeight = np.sum(currBoard*spotWeights)
 
+    # return linear combination of weights for a good heuristic
     return discWeight * -0.25 + spotWeight / 40 + moveWeight / 10
-
-
-def heuristic(currBoard: npBoard):
-    return len(npBoard.getLegalmoves(-1, currBoard))
 
 
 def findMax(gameboardArray, alpha, beta, currDepth, depthLimit):
@@ -153,24 +144,29 @@ def findMax(gameboardArray, alpha, beta, currDepth, depthLimit):
     :param alpha is the current alpha value
     :param beta is the current beta
     :param currDepth is the current depth of the search
-    :return currMin is the current minimum heuristic
+    :return currMin is the current minimum evaluation
     """
+    # we have reached the end of the tree, return evaluation value
     if currDepth == depthLimit:
         return evaluation(gameboardArray)
+
+    # worst case
     currMax = np.NINF
+
+    # see legal moves on max layer (us)
     legalMoves = npBoard.getLegalmoves(1, gameboardArray)
+
+    # return if legalMoves is empty
     if not legalMoves:
         return evaluation(gameboardArray)
-    # orderedMoves = orderMoves(gameboardArray, legalMoves)
+
+    # do the min layer
     for move in legalMoves:
-        # if str(np.append(gameboardArray, move)) in movesVisited:
-        #     continue
-        # movesVisited[str(np.append(gameboardArray, move))] = 1
         currMax = max(currMax, findMin(
             npBoard.set_piece_index(move, 1, gameboardArray), alpha, beta, currDepth+1, depthLimit))
-        if currMax >= beta:
+        if currMax >= beta:  # prune
             return currMax
-        alpha = max(alpha, currMax)
+        alpha = max(alpha, currMax)  # update alpha
     return currMax
 
 
@@ -181,38 +177,43 @@ def findMin(gameboardArray, alpha, beta, currDepth, depthLimit):
     :param alpha is the current alpha value
     :param beta is the current beta
     :param currDepth is the current depth of the search
-    :return currMax is the current maximum heuristic
+    :return currMax is the current maximum evaluation
     """
-    # if we already balls deep
+    # we have reached the end of the tree, return evaluation value
     if currDepth == depthLimit:
         return evaluation(gameboardArray)
 
+    # worst case
     currMin = np.inf
+
+    # see legal moves on min layer (opponent)
     legalMoves = npBoard.getLegalmoves(-1, gameboardArray)
+
     if not legalMoves:
         return evaluation(gameboardArray)
-    # explore the opontents counter moves to the one we were thinking of making
-    # orderedMoves = orderMoves(gameboardArray, legalMoves)
+
+    # prune the max layer
     for move in legalMoves:
-        # if str(np.append(gameboardArray, move)) in movesVisited:
-        #     continue
-        # movesVisited[str(np.append(gameboardArray, move))] = 1
         currMin = min(currMin, findMax(
             npBoard.set_piece_index(move, 1, gameboardArray), alpha, beta, currDepth+1, depthLimit))
         if currMin <= alpha:  # prune
             return currMin
-        beta = min(beta, currMin)
+        beta = min(beta, currMin)  # update beta
     return currMin
 
 
 def orderMoves(gameboardArray, moves: list):
+    """
+    Order the moves before pruning
+    :param gameboardArray is the gameboard
+    :param moves is the moves to be ordered
+    """
     ordered = []
     for move in moves:
-        ordered.append((move, evaluation(move)))
-    ordered.sort(key=lambda x: x[1], reverse=True)
+        # create tuples of index, evaluation
+        ordered.append((move, evaluation(gameboardArray)))
+    ordered.sort(key=lambda x: x[1], reverse=True)  # sort by heuristic value
     return ordered
 
 
-t1_start = process_time_ns()
 main()  # run code
-t1_stop = process_time_ns()
